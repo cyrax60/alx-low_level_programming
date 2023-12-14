0bb487ce96a4fe56ee0511f64b5bd8c84d694a7d
cyrax60 <ibm8313@gmail.com>
2023-12-14 08:42:09 -0800

    tsrano

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - this main function
+ *
+ * Descriptino: that program will print string
+ *
+ * Return: return 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
