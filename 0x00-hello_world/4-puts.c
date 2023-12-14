601803f4f06087a996962f8a2fd8687eacccd453
cyrax60 <ibm8313@gmail.com>
2023-12-14 08:02:17 -0800

    writting c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+ * main - the main function
+ * 
+ * discription: that program will print string
+ *
+ * return: return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
