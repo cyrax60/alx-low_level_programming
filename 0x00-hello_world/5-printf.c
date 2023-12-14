692c634dfef341bd5fbe87b4f4aeea4efa0c58d1
cyrax60 <ibm8313@gmail.com>
2023-12-14 08:14:25 -0800

    geitr

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - this is a main function
+ *
+ * Description: that prograrm will print string using printf function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
