91049729bdc7ae33f63ef9827cdd656c8ce7e528
cyrax60 <ibm8313@gmail.com>
2023-12-16 04:45:40 -0800

    srta

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdoi.h>
+/**
+ * main - this is the main fucntion
+ *
+ * Description: will print some string
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 9)
+		{
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ')0;
+		}
+	}
+	putchar('\');
+	return (0);
+}
