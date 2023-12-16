eed611faa8a7b6621f98534dfd7f1078737dd7a4
cyrax60 <ibm8313@gmail.com>
2023-12-16 04:10:26 -0800

    srata

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Description: will print all digits
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
