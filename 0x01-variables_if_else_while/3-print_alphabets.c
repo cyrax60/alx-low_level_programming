cb4474ddce0c1b09f8c7599d041f8349459e0845
cyrax60 <ibm8313@gmail.com>
2023-12-16 03:55:26 -0800

    trsatr

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - main funcitioon
+ *
+ * Description: will print some string
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int l, a;
+
+	for (l = 97; l <= 122; l++)
+	{
+		putchar(a);
+	}
+	for (a = 65; a <= 90; a++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
