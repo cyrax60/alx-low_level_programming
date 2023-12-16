d72c4f6e6d23f5b647f43ead7646109051bf9a86
cyrax60 <ibm8313@gmail.com>
2023-12-16 03:37:09 -0800

    tarst

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Description: will print alphabit ni lowercase
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar("\n");
+	return (0);
+}
