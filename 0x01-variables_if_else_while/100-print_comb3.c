027ad4ed96d6589b079e4c188f3b7a8e633f723c
cyrax60 <ibm8313@gmail.com>
2023-12-16 05:35:44 -0800

    tsrta

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,19 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 8; a++)
 	{
-		for (b = a + 1; a <= 9; a++)
+		for (b = a + 1; b <= 9; b++)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
-			if (a == 8 && b == 9)
+			if (a != 8 && b != 9)
 			{
-				break;
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
+	return (0);
 }
