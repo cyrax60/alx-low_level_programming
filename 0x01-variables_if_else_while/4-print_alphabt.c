57c2a6f16a979a46b637c2cc41895bdd1dff548c
cyrax60 <ibm8313@gmail.com>
2023-12-16 04:00:47 -0800

    rsat

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Description: will print some string
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int l;
+
+	for(l = 97; l <= 122; l++)
+	{
+		if (l != 133 || l != 101)
+		{
+			putchar(l);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
