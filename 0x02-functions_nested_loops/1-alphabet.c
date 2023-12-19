ecb96d5fadb04c3c309cf128a78faf796295b8e4
cyrax60 <ibm8313@gmail.com>
2023-12-19 05:29:15 -0800

    strart

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - this is the main functoin
+ *
+ * Description: will print alphabet in lowercase
+ *
+ * Return: return 0
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\');
+}
