1dc2de1e6d8778f38b03be050284df16dd93afcc
cyrax60 <ibm8313@gmail.com>
2023-12-19 05:15:49 -0800

    tsrtar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+ * main - this is the main function
+ *
+ * Description: will print socme string
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	_putcahr('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
