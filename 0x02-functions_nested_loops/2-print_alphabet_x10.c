c734ef4585e7aa08985c42df2e868252636a2fa0
cyrax60 <ibm8313@gmail.com>
2023-12-19 05:34:21 -0800

    sta

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - this is the function
+ *
+ * Description: will print some alphabet
+ *
+ * Return: return 0
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+	
+}
