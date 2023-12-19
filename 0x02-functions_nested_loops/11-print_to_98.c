3658d1ebe13cddbd8de08dcdedfb3a69d4de0937
cyrax60 <ibm8313@gmail.com>
2023-12-19 08:31:03 -0800

    srta

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - this is the function
+ *
+ * @n: this is paramter n
+ *
+ * Description: will print some digit
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+}
