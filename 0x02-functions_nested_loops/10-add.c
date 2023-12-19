4c1ec7478a7a3d9fc5f47460415f1cc060349111
cyrax60 <ibm8313@gmail.com>
2023-12-19 08:27:03 -0800

    strats

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - this the function
+ *
+ * @a: this is paramter a
+ *
+ * @b: this is paramter b
+ *
+ * Desciption: will print some digit
+ *
+ * Return: return 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
