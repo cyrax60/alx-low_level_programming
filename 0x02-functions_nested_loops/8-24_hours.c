2125555a34a27fd8a9a0cff5b8269c13e88d487a
cyrax60 <ibm8313@gmail.com>
2023-12-19 06:33:33 -0800

    tarsta

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,19 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int minut, hour;
 
-	for (a = 0; a <= 2; a++)
+	for (hour = 0; hour <= 24; hour++)
 	{
-		for (b = 0; b <= 4; b++)
+		for (minut = 0; minut <= 60; minut++)
 		{
-			for (c = 0; c <= 6; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minut / 10) + '0');
+			_putchar((minut % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
+
