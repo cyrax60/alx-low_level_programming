f8b1110c78835ebeb76f2e93b31dfe7d4639d5c8
cyrax60 <ibm8313@gmail.com>
2023-12-24 06:29:16 -0800

    tasrta

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isdigit - this is the function
+ *
+ * @c: this is the parameter
+ *
+ * Descirption: will pirnt some digit
+ *
+ * Return: return 0
+ */
+int _isdigit(int c)
+{
+	if ((c <= 9) && (c >= 0))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
