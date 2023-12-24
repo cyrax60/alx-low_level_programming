19facb9f45ccc3dafd194e8cfcf16a32b95d3dd7
cyrax60 <ibm8313@gmail.com>
2023-12-24 07:22:33 -0800

    tsrat

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,13 +12,7 @@ void print_line(int n)
 
 	for (a = 1; a <= n; a++)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
+	_putchar('\n');
 }
