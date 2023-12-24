1fc2ac2f6879c57fa9af604391234693c0c4d321
cyrax60 <ibm8313@gmail.com>
2023-12-24 06:36:34 -0800

    strtar

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - this is the function
+ *
+ * @a: this is the parameter a
+ * @b: this is the parameter b
+ *
+ * Description: will print some string
+ *
+ * Return: return 0
+ */
+int mul(int a, int b)
+{
+	int z;
+	
+	z = a + b;
+	return (z);
+}
