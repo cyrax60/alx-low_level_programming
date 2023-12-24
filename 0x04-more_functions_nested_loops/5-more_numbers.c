f8080e1d653123d785c31fcff4ebbc3e155e9833
cyrax60 <ibm8313@gmail.com>
2023-12-24 06:50:31 -0800

    tsrtar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - this is the function
+ *
+ * Description: will print some digit
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 1; b <= 14; b++)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
