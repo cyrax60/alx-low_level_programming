ab0cf7c1ca3059653f1f60a9c25ca89eff00214c
cyrax60 <ibm8313@gmail.com>
2024-01-02 06:53:55 -0800

    rtsat

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - this is the function
+ *
+ * @a: this is the parameter a
+ * @b: this is the parameter b
+ *
+ * Descripcion: will swap two numbers
+ */
+void swap_int(int *a, int *b)
+{
+	int z;
+	*a = z;
+	*a = *b;
+	*b = z;
+}
