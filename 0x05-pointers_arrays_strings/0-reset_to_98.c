2f8c111f8afbfb4fe9698b937cb01fdd7278a905
cyrax60 <ibm8313@gmail.com>
2024-01-02 06:40:28 -0800

    tsrtsar

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - this is the function
+ *
+ * @n: this is the parameter
+ *
+ * Description: will print some string
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
