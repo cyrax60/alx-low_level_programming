57654b207f49969b51d3e2f70462da9f5b0eb115
cyrax60 <ibm8313@gmail.com>
2024-01-04 12:10:41 -0800

    tsartsr

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - this is the function
+ *
+ * @s: this is parameter s
+ *
+ * Return: return a pointer
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
+
+}
