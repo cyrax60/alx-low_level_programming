678bc6a37b79f1f5603578091dab7b49591022a8
cyrax60 <ibm8313@gmail.com>
2024-01-09 05:00:33 -0800

    tsar

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - this is the function
+ *
+ * @dest: array of character
+ * @src: array of character
+ * @n: int
+ *
+ * Description: will cpy array
+ *
+ * Return: return array
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
