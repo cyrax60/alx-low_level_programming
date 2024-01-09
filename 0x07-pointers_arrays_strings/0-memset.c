0dacee786c62ef015177cd37f7a05dca97e24fb0
cyrax60 <ibm8313@gmail.com>
2024-01-09 04:28:30 -0800

    sartar

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - this is the function
+ *
+ * @s: pointer
+ * @b: character
+ * @n: int
+ *
+ * Description: will mem set
+ *
+ * Return: return pointer or array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
