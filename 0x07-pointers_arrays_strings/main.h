2e74710be4cf5ee784b5d9e7a59891ab61505c21
cyrax60 <ibm8313@gmail.com>
2024-01-09 04:45:34 -0800

    tsrata

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,8 @@
-#ifndef MAIN.H
-#define MAIN.H
-
-#include <stddef.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
