9ba41b529e2815b11cee5af039b45bbc36648459
cyrax60 <ibm8313@gmail.com>
2024-01-12 07:33:14 -0800

    trsata

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strpbrk - this is the function
+ *
+ * @s: pointer
+ * @accept: pointer
+ *
+ * Descriptoin: will print string
+ *
+ * Return: return pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+	char *ptr;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (accept[y] == s[x])
+			{
+				ptr = &s[x];
+				return (ptr);
+			}
+		}
+	}
+	return (0);
+}
