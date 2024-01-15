86f30b7d87c600bfd7a79323dd597c2f5ccca22a
cyrax60 <ibm8313@gmail.com>
2024-01-15 05:08:30 -0800

    tsat

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
