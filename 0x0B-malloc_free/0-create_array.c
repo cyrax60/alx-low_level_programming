a0c0edff10dbeea5c1587c10fc6e533f4a8f4b06
cyrax60 <ibm8313@gmail.com>
2024-01-16 04:09:31 -0800

    tsrtrar

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * creat_array - function
+ * @size: int
+ * @c: char
+ * Return: return char
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (size != 0)
+	{
+		array = (char *)malloc(size * sizeof(char));
+		for (i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
+	}
+	return (array);
+}
